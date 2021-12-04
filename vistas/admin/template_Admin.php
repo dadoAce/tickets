224cfb8fbff590e8b78f3dde03497923d8c2316f
Alfonso Acevedo <Eduardo_acevedo@grupoiikno.com>
2021-12-04 14:44:36 -0600

    cambios

--- vistas/admin/template_Admin.php
+++ vistas/admin/template_Admin.php
@@ -3,8 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>Tickets</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= $this->vista("complementos/referencias/referencias"); ?>
+    <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
 </head>
 
 <body>
@@ -12,16 +14,16 @@
 
         <!--MENU LATERAL-->
         <?php $this->vista("complementos/menus/menuPrincipal"); ?>
-        <section id="contenido" class=" d-flex flex-column justify-content-center align-items-center     pl-2 pr-2">
+        <section id="contenido" class="d-flex flex-column justify-content-center align-items-center     pl-2 pr-2">
             <div id="content-titulo" class="w-100 p-3 d-flex justify-content-between ">
-                <h6>Submitted Tickets</h6>
-                <form action="<?= $this->base_url("Usuario/cerrarSesion") ?>" method="GET" style="margin-bottom: -0.5rem;">
-                    <input type="submit" value="Log Out" class="btn">
+                <h6 class="m-0 p-0 d-flex justify-content-end align-items-center">Submitted Tickets</h6>
+                <form action="<?= $this->base_url("Usuario/cerrarSesion") ?>" method="GET" class="m-0 p-0 d-flex justify-content-end align-items-end">
+                    <input type="submit" value="Log Out" class="btn p-0">
                 </form>
             </div>
-            <div class="pr-3 pl-3 w-100">
+            <div class="pr-3 pl-3 w-100 pt-3">
 
-                <table class=" table w-100">
+                <table id="tablaTickets" class=" table table-responsive-sm">
                     <thead>
                         <tr>
 
@@ -65,5 +67,10 @@
 </body>
 
 </html>
-
 <?= $this->vista("complementos/referencias/referencias_footer"); ?>
+<script type="text/javascript" src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#tablaTickets').DataTable();
+    });
+</script>
